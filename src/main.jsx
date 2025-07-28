54fc0180d395ca911182df1d3ba11ad29d622edf
ara-shin <ara.shin@seerstech.com>
2025-07-28 10:46:26 +0900

    -

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
 import './styles/font.css';
 import './styles/reset.css';
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-)
+  <BrowserRouter basename="/control-system">
+    <App />
+  </BrowserRouter>
+);
